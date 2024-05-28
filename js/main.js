18f122f752314518f7c3104a1ebe77a4d90dbf0f
unknown <melancholia200312@gmail.com>
2024-05-29 00:39:56 +0400

    animated path

--- js/main.js
+++ js/main.js
@@ -9,16 +9,31 @@ $('.part').hover (
 )
 
 
-$(".part").on("mouseover",function(){
-    $(".quest-path").css({
+$("#quest-1").on("mouseover",function(){
+    $(".quest-path-1").css({
         opacity: 1,
 	transition : 'opacity 0.25s ease-in-out'
     })
 });
 
 
-$(".part").on("mouseout",function(){
-    $(".quest-path").css({
+$("#quest-1").on("mouseout",function(){
+    $(".quest-path-1").css({
+        opacity: 0
+    })
+});
+
+
+$("#quest-2").on("mouseover",function(){
+    $(".quest-path-2").css({
+        opacity: 1,
+	transition : 'opacity 0.25s ease-in-out'
+    })
+});
+
+
+$("#quest-2").on("mouseout",function(){
+    $(".quest-path-2").css({
         opacity: 0
     })
 });
